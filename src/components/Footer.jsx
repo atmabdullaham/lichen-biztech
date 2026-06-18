b715fe2a8294627c5d3d8304c47515fd4d786119
dev-rakibul-islam <rakibulislam.eb@gmail.com>
2026-06-18 13:01:59 +0600

    change on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ const Footer = () => {
   return (
     <footer>
       <div className="container mx-auto py-4 text-center text-sm text-gray-500">
-        &copy; {new Date().getFullYear()} Lichen. All rights reserved.
+        &copy; {new Date().getFullYear()} Lichen. All rights reserved. Develop
+        by{" "}
+        <a
+          href="abdelrahman-lichi.vercel.app"
+          className="text-blue-500 hover:underline"
+        >
+          Abdelrahman Lichi
+        </a>
       </div>
     </footer>
   );
